6dbc7d2b4aa2a334d0ad682b8a100b627a2f59b4
Matt Novenstern <fisxoj@gmail.com>
2023-11-22 23:41:58 -0500

    scripts/build-image.sh: Rework to build locally
    Probably needs more work...

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,7 +2,7 @@
 
 # This file will only work when called by the Makefile, which sets some variables
 
-function tag () {
+function release_tag () {
     echo "${SBCL_VERSION}-${QUICKLISP_DIST}"
 }
 
@@ -13,10 +13,8 @@ cd ${VARIANT}
 ORGANIZATION=parentheticalenterprises
 
 BASE_NAME=${BASE_NAME:-sbcl-quicklisp}
-
-VERSIONED_TAG="${ORGANIZATION}/${BASE_NAME}-${VARIANT}:$(tag)"
-LATEST_TAG="${ORGANIZATION}/$BASE_NAME-${VARIANT}:latest"
-CI_TAG="${ORGANIZATION}/${BASE_NAME}-${VARIANT}:${TRAVIS_COMMIT}"
+IMAGE_NAME="${ORGANIZATION}/${BASE_NAME}-${VARIANT}"
+VERSION=$(git rev-parse --short HEAD 2>/dev/null)
 
 # For me, locally!
 shopt -s expand_aliases
@@ -30,10 +28,12 @@ if [ ! -z ${TRAVIS_COMMIT+x} ]; then
     echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USER --password-stdin
 
     TAG=$CI_TAG
+    BASE_TAG="$(release_tag)"
 else
     echo "Not logging into docker repo because we aren't in CI."
 
     TAG=$LATEST_TAG
+    BASE_TAG="${VERSION}"
 fi
 
 case "$1" in
@@ -41,9 +41,11 @@ case "$1" in
 	docker build \
                --build-arg SBCL_VERSION=${SBCL_VERSION} \
                --build-arg QUICKLISP_DIST=${QUICKLISP_DIST} \
-               --build-arg TAG=${TRAVIS_COMMIT} \
+               --build-arg BASE_TAG=${BASE_TAG} \
                . \
-               -t "${TAG}"
+               -t "${IMAGE_NAME}:${VERSION}" \
+               -t "${IMAGE_NAME}:latest"
+
 
         if [ ! -z ${TRAVIS_COMMIT+x} ]; then
             echo "Pushing CI version of image"
@@ -57,8 +59,8 @@ case "$1" in
         ;;
     publish)
         docker pull "${TAG}"
-        docker tag "${TAG}" "${VERSIONED_TAG}"
-	docker push "${VERSIONED_TAG}"
-        docker tag "${TAG}" "${LATEST_TAG}"
-	docker push "${LATEST_TAG}"
+        docker tag "${TAG}" "${IMAGE_NAME}:$(release_tag)"
+	docker push "${IMAGE_NAME}:$(release_tag)"
+        docker tag "${TAG}" "${IMAGE_NAME}:LATEST"
+	docker push "${IMAGE_NAME}:LATEST"
 esac
